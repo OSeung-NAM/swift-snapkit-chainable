30d79390f79d5111fc90dea1bf3407df5beacc7d
남오승 <developernam92@gmail.com>
2021-07-09 17:59:23 +0900

    수정 * Previews 부분 UIView 선언부로 변경

--- snapkit-chainable/ViewController+UI.swift
+++ snapkit-chainable/ViewController+UI.swift
@@ -13,9 +13,38 @@ extension ViewController {
             .add(to: view)
             .background(color: .yellow)
             .constrain({ maker in
-                maker.width.equalTo(400)
-                maker.height.equalTo(400)
+                maker.width.equalTo(200)
+                maker.height.equalTo(200)
                 maker.center.equalToSuperview()
             }).origin
     }
 }
+
+
+/*
+ UIKit -> SwiftUI 변환
+ Xcode Previews 기능
+ */
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+@available(iOS 13.0, *)
+struct MainVcRepresentble: UIViewRepresentable {
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<MainVcRepresentble>) {
+        print("updateUIView")
+    }
+    
+    func makeUIView(context: Context) -> UIView { ViewController().view }
+    
+}
+@available(iOS 13.0, *)
+struct MainVcPreview: PreviewProvider {
+    static var previews: some View { Group {
+        MainVcRepresentble()
+            .previewDevice("iPhone 12 mini")
+        
+        MainVcRepresentble()
+            .previewDevice("iPhone 11 Pro Max")
+    } }
+}
+#endif
+ 
--- snapkit-chainable/ViewController.swift
+++ snapkit-chainable/ViewController.swift
@@ -19,31 +19,3 @@ class ViewController: UIViewController {
 
     }
 }
-
-/*
- UIKit -> SwiftUI 변환
- Xcode Previews 기능
- */
-#if canImport(SwiftUI) && DEBUG
-import SwiftUI
-@available(iOS 13.0, *)
-struct MainVcRepresentble: UIViewRepresentable {
-    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<MainVcRepresentble>) {
-        print("updateUIView")
-    }
-    
-    func makeUIView(context: Context) -> UIView { ViewController().view }
-    
-}
-@available(iOS 13.0, *)
-struct MainVcPreview: PreviewProvider {
-    static var previews: some View { Group {
-        MainVcRepresentble()
-            .previewDevice("iPhone 12 mini")
-        
-        MainVcRepresentble()
-            .previewDevice("iPhone 11 Pro Max")
-    } }
-}
-#endif
- 
