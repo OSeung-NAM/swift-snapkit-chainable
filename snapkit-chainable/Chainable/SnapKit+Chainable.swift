fe0ba9f854d67e2ae21b82c6eded49fb3ef45135
남오승 <developernam92@gmail.com>
2021-07-11 22:45:40 +0900

    수정 * chainable 수정

--- snapkit-chainable/Chainable/SnapKit+Chainable.swift
+++ snapkit-chainable/Chainable/SnapKit+Chainable.swift
@@ -7,15 +7,41 @@
 
 import SnapKit
 import UIKit
+import RxSwift
+import RxCocoa
+
+extension Reactive where Base: UIView {
+    var didMoveToSuperview: ControlEvent<Void> {
+        let event = methodInvoked(#selector(Base.didMoveToSuperview)).map { _ in }
+        return ControlEvent(events: event)
+    }
+}
 
 extension Chain where Origin: UIView {
-    func constrain(_ maker: (ConstraintMaker) -> Void) -> Chain {
-        origin.snp.makeConstraints(maker)
+    func constrain(_ maker: @escaping (ConstraintMaker) -> Void) -> Chain {
+        if let _ = origin.superview {
+            origin.snp.makeConstraints(maker)
+        }else {
+            _ = origin.rx.didMoveToSuperview
+                .take(1)
+                .subscribe(onNext:{ [weak self] _ in
+                    self?.origin.snp.makeConstraints(maker)
+                })
+        }
+        
         return self
     }
     
-    func updateConstrain(_ maker: (ConstraintMaker) -> Void) -> Chain {
-        origin.snp.updateConstraints(maker)
+    func updateConstrain(_ maker: @escaping (ConstraintMaker) -> Void) -> Chain {
+        if let _ = origin.superview {
+            origin.snp.updateConstraints(maker)
+        }else {
+            _ = origin.rx.didMoveToSuperview
+                .take(1)
+                .subscribe(onNext:{ [weak self] _ in
+                    self?.origin.snp.updateConstraints(maker)
+                })
+        }
         return self
     }
 }
